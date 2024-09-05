ccfc6a5b1b1e60077eec11f33662d8c799600e73
Marcos Paulo <marcos.poo@gmail.com>
2024-09-05 15:29:23 -0300

    Aula 2.1 - Definindo ações para o filme

--- screen-match/index.php
+++ screen-match/index.php
@@ -1,66 +1,20 @@
 <?php
 
 require __DIR__ . "/src/Model/Filme.php";
-include __DIR__ . "/src/funcoes.php";
 
-echo "Bem-vindo(a) ao screen match!\n";
+echo "Bem-vindo(a) ao ScreenMatch\n";
 
-$nomeFilme = "Top Gun - Maverick";
+$filme = new Filme();
+$filme->nome = 'Thor - Ragnarok';
+$filme->anoLancamento = 2021;
+$filme->genero = 'super-heroi';
 
-$anoLancamento = 2022;
+$filme->avalia(10);
+$filme->avalia(10);
+$filme->avalia(5);
+$filme->avalia(5);
 
-$quantidadeDeNotas = $argc - 1;
-$notas = [];
-
-for ($contador = 1; $contador < $argc; $contador++) {
-    $notas[] = (float) $argv[$contador];
-}
-
-$notaFilme = array_sum($notas) / $quantidadeDeNotas;
-$planoPrime = true;
-
-$incluidoNoPlano = incluidoNoPlano($planoPrime, $anoLancamento);
-
-echo "Nome do filme: " . $nomeFilme . "\n";
-echo "Nota do filme: $notaFilme\n";
-echo "Ano de lançamento: $anoLancamento\n";
-
-exibeMensagemLancamento($anoLancamento);
-
-$genero = match ($nomeFilme) {
-    "Top Gun - Maverick" => "ação",
-    "Thor: Ragnarok" => "super-herói",
-    "Se beber não case" => "comédia",
-    default => "gênero desconhecido",
-};
-
-echo "O gênero do filme é: $genero\n";
-
-$filme = criaFilme(
-    nome: "Thor: Ragnarok", 
-    anoLancamento: 2021, 
-    nota: 7.8, 
-    genero: "super-herói"
-);
-
-echo $filme->anoLancamento;
-
-var_dump($notas);
-sort($notas);
-var_dump($notas);
-
-echo min($notas) . " é a menor notas" . "\n";
-echo max($notas) . " é a maior notas" . "\n";
-
-var_dump($filme->nome);
-$posicaoDoisPontos = strpos($filme->nome, ':');
-var_dump($posicaoDoisPontos);
-
-var_dump(substr($filme->nome, 0, $posicaoDoisPontos));
-
-//echo json_encode($filme);
-//var_dump(json_decode('{"nome":"Thor: Ragnarok","ano":2021,"nota":7.8,"genero":"super-her\u00f3i"}', true));
+var_dump($filme);
 
 
-$filmeComoStringJson = json_encode($filme);
-file_put_contents(__DIR__ . '/filme.json', $filmeComoStringJson);
+echo $filme->media();
--- screen-match/src/Model/Filme.php
+++ screen-match/src/Model/Filme.php
@@ -4,5 +4,19 @@ class Filme {
     public string $nome;
     public int $anoLancamento;
     public string $genero;
-    public float $nota;
+    public float $media;
+    public array $notas = [];
+
+    function avalia(float $nota): void
+    {
+        $this->notas[] = $nota;
+    }
+
+    function media(): float 
+    {
+        $somaNotas = array_sum($this->notas);
+        $quantidadeNotas = count($this->notas);
+
+        return $somaNotas / $quantidadeNotas;
+    }
 }
